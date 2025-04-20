9e871be42611a8fca134e5de4599e0baf864b236
devsujalpatel <sujal7455@gmail.com>
2025-04-21 00:33:02 +0530

    database setup and user model created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv"
 
-dotenv.config()
+dotenv.config({
+    path: './.env'
+})
 
 const app = express()
 app.use(express.json())
 
 const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
 
 app.listen(port, () => {
     console.log("server is listening at port:", port);
