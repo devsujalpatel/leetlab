7f4bfbed5f3ef8e5afd7253b6a1f23422274c36a
devsujalpatel <sujal7455@gmail.com>
2025-04-21 00:59:47 +0530

    register user done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,24 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-dotenv.config({
-    path: './.env'
-})
+import authRoutes from "./routes/auth.routes.js";
 
-const app = express()
-app.use(express.json())
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser())
 
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+	res.send("Hello Welcome to leetlab 🔥");
+});
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(port, () => {
-    console.log("server is listening at port:", port);
-})
+	console.log("server is listening at port:", port);
+});
