c1d4448507f648aeec17aaf447cdf610692b0b0a
devsujalpatel <sujal7455@gmail.com>
2025-04-21 01:51:08 +0530

    logout user done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -37,6 +37,7 @@ export const register = async (req, res) => {
 			maxAge: 1000 * 60 * 60 * 24 * 7,
 		});
 		res.status(201).json({
+			success: true,
 			message: "User created successfully",
 			user: {
 				id: newUser.id,
@@ -84,6 +85,7 @@ export const login = async (req, res) => {
 			maxAge: 1000 * 60 * 60 * 24 * 7,
 		});
 		res.status(201).json({
+			success: true,
 			message: "User Loggedin successfully",
 			user: {
 				id: user.id,
@@ -100,5 +102,24 @@ export const login = async (req, res) => {
 		});
 	}
 };
-export const logout = async (req, res) => {};
-export const check = async (req, res) => {};
+export const logout = async (req, res) => {
+	try {
+		res.clearCookie("jwt", {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: process.env.NODE_ENV !== "development",
+		});
+		res.status(200).json({
+			success: true,
+			message: "User loged out successfully"
+		})
+	} catch (error) {
+		console.error("Error logout user:", error);
+		res.status(500).json({
+			error: "Error logout user",
+		});
+	}
+};
+export const check = async (req, res) => {
+	
+};
